6c11a1940374153a06453037904214c90149a1d2
flyindemon <104937168+flyindemon@users.noreply.github.com>
2024-05-14 21:12:05 +0200

    Changed the DJOnair to use ajax fetch

--- assets/javascript/djonair.js
+++ assets/javascript/djonair.js
@@ -6,15 +6,15 @@ function isValidUrl(url) {
     } catch (error) {
         return false;
     }
-  }
-  
-  function updateDJInfo() {
+}
+
+async function updateDJInfo() {
     // Fetch URL for XerosRadio Api
     const url = 'https://azuracast.streamxerosradio.duckdns.org/api/nowplaying_static/xerosradio.json';
-  
+
     const djInfoElement = document.getElementById('djInfo');
     const artworkElement = document.getElementById('artwork');
-  
+
     const fetchOptions = {
         method: 'GET',
         mode: 'cors', // Cors for XerosRadio Api
@@ -23,54 +23,51 @@ function isValidUrl(url) {
             'Content-Type': 'application/json',
         },
     };
-  
-    fetch(url, fetchOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Het verzoek aan de XerosRadio Servers is mislukt. Probeer het later opnieuw.');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.live && data.live.is_live) {
-                djInfoElement.textContent = data.live.streamer_name;
-                const artworkUrl = isValidUrl(data.live.art)
-                    ? data.live.art
-                    : 'https://res.cloudinary.com/xerosradio/image/upload/w_200,h_200,f_auto,q_auto/XerosRadio_Logo_Achtergrond_Wit';
-  
-                const newImage = new Image();
-                newImage.src = "https://wsrv.nl/?w=200&h=200&output=webp&url=" + artworkUrl;
-                newImage.draggable = false; // Prevent image dragging | XerosRadio Api
-                newImage.loading = "lazy";
-                newImage.alt = "DJ";
-                newImage.style.opacity = 1;
-                newImage.style.width = '200px';
-                newImage.style.height = '200px';
-  
-                // Disable right-click context menu
-                newImage.addEventListener('contextmenu', function (e) {
-                    e.preventDefault();
-                });
-  
-                artworkElement.innerHTML = '';
-                artworkElement.appendChild(newImage);
-            } else {
-                djInfoElement.textContent = 'Nonstop Muziek';
-                artworkElement.innerHTML = `<img src="https://res.cloudinary.com/xerosradio/image/upload/w_200,h_200,f_auto,q_auto/v1/Assets/Nonstop_Muziek" alt="XerosRadio" draggable="false" loading="lazy" style="width: 200px; height: 200px;">`;
-            }
-        })
-        .catch(error => {
-            console.error('Fout:', error);
-            const djInfoElement = document.getElementById('djInfo');
-            djInfoElement.textContent = 'XerosRadio is momenteel niet beschikbaar. Probeer het later opnieuw.';
-  
-            // Load the default image on error of XerosRadio Api
-            const artworkElement = document.getElementById('artwork');
-            artworkElement.innerHTML = `<img src="https://res.cloudinary.com/xerosradio/image/upload/w_200,h_200,f_auto,q_auto/XerosRadio_Logo_Achtergrond_Wit" alt="XerosRadio" draggable="false" loading="lazy" style="width: 200px; height: 200px;">`;
-        });
-  }
-  
-  // Get New DJ-info immediately From XerosRadio Api and check and if avabile load every 5 seconds
-  setInterval(updateDJInfo, 5000);
-  updateDJInfo(); // Call the function immediately
-  
+
+    try {
+        const response = await fetch(url, fetchOptions);
+
+        if (!response.ok) {
+            throw new Error('Het verzoek aan de XerosRadio Servers is mislukt. Probeer het later opnieuw.');
+        }
+
+        const data = await response.json();
+
+        if (data.live && data.live.is_live) {
+            djInfoElement.textContent = data.live.streamer_name;
+            const artworkUrl = isValidUrl(data.live.art)
+                ? data.live.art
+                : 'https://res.cloudinary.com/xerosradio/image/upload/w_200,h_200,f_auto,q_auto/XerosRadio_Logo_Achtergrond_Wit';
+
+            const newImage = new Image();
+            newImage.src = "https://wsrv.nl/?w=200&h=200&output=webp&url=" + artworkUrl;
+            newImage.draggable = false; // Prevent image dragging | XerosRadio Api
+            newImage.loading = "lazy";
+            newImage.alt = "DJ";
+            newImage.style.opacity = 1;
+            newImage.style.width = '200px';
+            newImage.style.height = '200px';
+
+            // Disable right-click context menu
+            newImage.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+            });
+
+            artworkElement.innerHTML = '';
+            artworkElement.appendChild(newImage);
+        } else {
+            djInfoElement.textContent = 'Nonstop Muziek';
+            artworkElement.innerHTML = `<img src="https://res.cloudinary.com/xerosradio/image/upload/w_200,h_200,f_auto,q_auto/v1/Assets/Nonstop_Muziek" alt="XerosRadio" draggable="false" loading="lazy" style="width: 200px; height: 200px;">`;
+        }
+    } catch (error) {
+        console.error('Fout:', error);
+        djInfoElement.textContent = 'XerosRadio is momenteel niet beschikbaar. Probeer het later opnieuw.';
+
+        // Load the default image on error of XerosRadio Api
+        artworkElement.innerHTML = `<img src="https://res.cloudinary.com/xerosradio/image/upload/w_200,h_200,f_auto,q_auto/XerosRadio_Logo_Achtergrond_Wit" alt="XerosRadio" draggable="false" loading="lazy" style="width: 200px; height: 200px;">`;
+    }
+}
+
+// Get New DJ-info immediately From XerosRadio Api and check and if available load every 5 seconds
+setInterval(updateDJInfo, 5000);
+updateDJInfo(); // Call the function immediately
