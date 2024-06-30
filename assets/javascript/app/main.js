27ba2807664151fb1b31282acf1a5250b343acba
flyindemon <104937168+flyindemon@users.noreply.github.com>
2024-06-30 20:08:44 +0200

    Update main.js

--- assets/javascript/app/main.js
+++ assets/javascript/app/main.js
@@ -1,24 +1,22 @@
-// Bescherming tegen afbeeldingsdiefstal | XerosRadio
 document.addEventListener('DOMContentLoaded', function () {
     // Selecteer alle afbeeldingen op de pagina
     const afbeeldingen = document.querySelectorAll('img');
   
-    // Voorkom alles met de Content | XerosRadio
-    function toonAangepoptBericht(event) {
-        event.preventDefault();
-    }
-  
-    // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan | XerosRadio
+    // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan
     afbeeldingen.forEach((img) => {
-        img.addEventListener('contextmenu', toonAangepoptBericht);
-        
-        // Voorkom slepen en kopiëren van afbeeldingen | XerosRadio
+        // Voorkom slepen en kopiëren van afbeeldingen
         img.setAttribute('draggable', 'false');
         img.addEventListener('dragstart', function (e) {
             e.preventDefault();
         });
     });
 });
+
+// Laad de juiste scroll hoogte van het scherm
+window.onload = function() {
+    var chatBox = document.getElementById('chatBox');
+    chatBox.scrollTop = chatBox.scrollHeight;
+};
 
 // Laad de juiste scroll hoogte van het scherm
 window.onload = function() {
