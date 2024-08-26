997fc258ac029571c5d3de214113083519d59058
flyindemon <104937168+flyindemon@users.noreply.github.com>
2024-08-26 21:23:21 +0200

    Update main.js
    + Changed it back to the script that worked
    - Removed Old Script
    - this one is special because it has the app text and not website!

--- assets/javascript/app/main.js
+++ assets/javascript/app/main.js
@@ -1,25 +1,22 @@
+// Bescherming tegen afbeeldingsdiefstal | XerosRadio
 document.addEventListener('DOMContentLoaded', function () {
     // Selecteer alle afbeeldingen op de pagina
     const afbeeldingen = document.querySelectorAll('img');
   
-    // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan
+    // Voorkom het standaard contextmenu en toon een aangepopt bericht | XerosRadio
+    function toonAangepoptBericht(event) {
+      event.preventDefault();
+      alert('U mag geen afbeeldingen van onze app stelen zonder toestemming!');
+    }
+  
+    // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan | XerosRadio
     afbeeldingen.forEach((img) => {
-        // Voorkom slepen en kopiëren van afbeeldingen
-        img.setAttribute('draggable', 'false');
-        img.addEventListener('dragstart', function (e) {
-            e.preventDefault();
-        });
+      img.addEventListener('contextmenu', toonAangepoptBericht);
+      
+      // Voorkom slepen en kopiëren van afbeeldingen | XerosRadio
+      img.setAttribute('draggable', 'false');
+      img.addEventListener('dragstart', function (e) {
+        e.preventDefault();
+      });
     });
-});
-
-// Laad de juiste scroll hoogte van het scherm
-window.onload = function() {
-    var chatBox = document.getElementById('chatBox');
-    chatBox.scrollTop = chatBox.scrollHeight;
-};
-
-// Laad de juiste scroll hoogte van het scherm
-window.onload = function() {
-    var chatBox = document.getElementById('chatBox');
-    chatBox.scrollTop = chatBox.scrollHeight;
-};
+  });
