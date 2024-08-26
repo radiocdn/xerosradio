879d2bd4edd6cfb536df75c4c511e31759cc908c
flyindemon <104937168+flyindemon@users.noreply.github.com>
2024-08-26 22:55:02 +0200

    Update main.js
    organized the code

--- assets/javascript/app/main.js
+++ assets/javascript/app/main.js
@@ -2,29 +2,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Selecteer alle afbeeldingen op de pagina
     const afbeeldingen = document.querySelectorAll('img');
-  
+
     // Voorkom het standaard contextmenu en toon een aangepopt bericht | XerosRadio
     function toonAangepoptBericht(event) {
-      event.preventDefault();
-      alert('U mag geen afbeeldingen van onze app stelen zonder toestemming!');
+        event.preventDefault();
+        alert('U mag geen afbeeldingen van onze app stelen zonder toestemming!');
     }
-  
+
     // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan | XerosRadio
     afbeeldingen.forEach((img) => {
-      img.addEventListener('contextmenu', toonAangepoptBericht);
-      
-      // Voorkom slepen en kopiëren van afbeeldingen | XerosRadio
-      img.setAttribute('draggable', 'false');
-      img.addEventListener('dragstart', function (e) {
-        e.preventDefault();
-      });
+        img.addEventListener('contextmenu', toonAangepoptBericht);
+
+        // Voorkom slepen en kopiëren van afbeeldingen | XerosRadio
+        img.setAttribute('draggable', 'false');
+        img.addEventListener('dragstart', function (e) {
+            e.preventDefault();
+        });
     });
-  });
-    (window.onload = function () {
-        var t = document.getElementById("chatBox");
-        t.scrollTop = t.scrollHeight;
-    }),
-    (window.onload = function () {
-        var t = document.getElementById("chatBox");
-        t.scrollTop = t.scrollHeight;
-    });
+});
+
+// Scroll automatisch naar het einde van de chatbox na het laden | XerosRadio
+window.onload = function () {
+    var chatBox = document.getElementById("chatBox");
+    if (chatBox) {
+        chatBox.scrollTop = chatBox.scrollHeight;
+    }
+};
