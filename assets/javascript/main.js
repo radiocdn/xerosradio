8a76ca8e5bbc445cbb2207cd8fbb1cb05600a444
flyindemon <104937168+flyindemon@users.noreply.github.com>
2025-01-16 17:50:31 +0100

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,22 +1,29 @@
-// Bescherming tegen afbeeldingsdiefstal | XerosRadio
 document.addEventListener('DOMContentLoaded', function () {
     // Selecteer alle afbeeldingen op de pagina
     const afbeeldingen = document.querySelectorAll('img');
-  
+
     // Voorkom het standaard contextmenu en toon een aangepopt bericht | XerosRadio
     function toonAangepoptBericht(event) {
-      event.preventDefault();
-      alert('U mag geen afbeeldingen van onze website stelen zonder toestemming!');
+        event.preventDefault();
+        if (typeof Swal !== 'undefined') {
+            Swal.fire({
+                title: "Abeeldingen zijn beschermd!",
+                text: "Het is niet toegestaan om Afbeeldingen te Downloaden",
+                icon: "error",
+            });
+        } else {
+            alert("Afbeeldingen zijn beschermd!"); // Fallback als Swal niet beschikbaar is
+        }
     }
-  
+
     // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan | XerosRadio
     afbeeldingen.forEach((img) => {
-      img.addEventListener('contextmenu', toonAangepoptBericht);
-      
-      // Voorkom slepen en kopiëren van afbeeldingen | XerosRadio
-      img.setAttribute('draggable', 'false');
-      img.addEventListener('dragstart', function (e) {
-        e.preventDefault();
-      });
+        img.addEventListener('contextmenu', toonAangepoptBericht);
+
+        // Voorkom slepen en kopiëren van afbeeldingen | XerosRadio
+        img.setAttribute('draggable', 'false');
+        img.addEventListener('dragstart', function (e) {
+            e.preventDefault();
+        });
     });
-  });
+});
